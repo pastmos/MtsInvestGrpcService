1509fbba80b737064daee70e25728179cb7bc061
Passt <pastspam@yandex.ru>
2021-07-01 15:39:40 +0300

    init made public

--- MtsInvestGrpcService/Classes/GRPCClass.swift
+++ MtsInvestGrpcService/Classes/GRPCClass.swift
@@ -10,7 +10,7 @@ import Foundation
 open class MtsGRPCClass {
     var testVariable: Int
     
-    init(testVariable: Int) {
+    public init(testVariable: Int) {
         self.testVariable = testVariable
     }
 }
